b4b2784c7303f8fafd17b65833041bc204b3c7c4
Kavan Soleimanbeigi <solxiom@gmail.com>
2014-12-09 17:20:30 +0200

    code refactored, controller is much simpler and logic is done in a service now

--- js/assignment.js
+++ js/assignment.js
@@ -1,28 +1,64 @@
 /**
  * Created by Kavan Soleimanbeigion 9.12.2014.
  */
-angular.module("assignmentApp", []).controller("appCtrl",function($scope){
-    $scope.strLength = 0;
-    $scope.words =  0;
-    $scope.reverse = "";
-    $scope.string ="";
-//    $scope.updateOutput();
-    $scope.updateOutput = function(){
-        $scope.strLength = $scope.string.length;
-        $scope.words = wordCount();
-        $scope.reverse = $scope.string.split("").reverse().join("");;
-    };
-    function wordCount(){
-        return $scope.string.split(" ").length;
-    }
-}).directive('ngKeypress', function () {
-    return function (scope, element, attrs) {
-        element.bind("keydown keypress", function (event) {
+angular.module("assignmentApp", [])
+    .controller("appCtrl", function ($scope, assignmentSvc) {
 
-                scope.$apply(function (){
+        assignmentSvc.updateScope($scope);
+
+        $scope.updateOutput = function () {
+            assignmentSvc.updateScope($scope);
+        };
+    })
+    .factory("assignmentSvc", function () {
+
+        function updateScopeImpl(scope) {
+            if (typeof scope === 'undefined') {
+                return;
+            }
+            return typeof scope.string !== "undefined" && scope.string.length > 0 ?
+                setvariables(scope) : init(scope);
+        }
+
+        function setvariables(scope) {
+            scope.strLength = scope.string.length;
+            scope.words = wordCount(scope);
+            scope.reverse = scope.string.split("").reverse().join("");
+        }
+
+        function init(scope) {
+            scope.strLength = 0;
+            scope.words = 0;
+            scope.reverse = "";
+        }
+
+        function wordCount(scope) {
+            return scope.string.split(" ").length;
+        }
+
+        return{
+            /**
+             * interface for this service has only function
+             * notice: if the input value is empty
+             * this will set the view variables to 0 and empty
+             * @param scope
+             * @returns { scope.strLength, scope.words, scope.reverse}
+             */
+            updateScope: function (scope) {
+                return updateScopeImpl(scope);
+            }
+        }
+    })
+/**
+ * a simple angular directive for catching user keyPress events
+ */
+    .directive('ngKeypress', function () {
+        return function (scope, element, attrs) {
+            element.bind("keydown keypress", function () {
+                scope.$apply(function () {
                     scope.$eval(attrs.ngKeypress);
                 });
 
-        });
-    };
-});
+            });
+        };
+    });
